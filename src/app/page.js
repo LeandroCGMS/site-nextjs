b5714d0cc5e83e31aa82105a99d3aa756c324aee
LeCGMS <leandrocgms1986@outlook.com>
2025-04-24 22:03:56 -0400

    Mensagem automática do script "enviar" em 24/04/2025, às 22:03:56

--- src/app/page.js
+++ src/app/page.js
@@ -1,64 +1,85 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+
 
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-6 text-center text-gray-800">
-          Redefinir Senha
-        </h1>
+	const [id, setId] = useState("");
+	const [hash, setHash] = useState("");
+	const [password, setPassword] = useState("");
+	const [passwordConfirm, setPasswordConfirm] = useState("");
+	return (
+		<div className="min-h-screen bg-gray-100 flex items-center justify-center">
+			<div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md">
+				<h1 className="text-2xl font-bold mb-6 text-center text-gray-800">
+					Redefinir Senha
+				</h1>
 
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Id de Usuário
-            </label>
-            <input
-              type="text"
-              className="mt-1 block w-full px-4 py-2 border rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Ex: 123"
-            />
-          </div>
+				<div>
+					<label className="block text-sm font-medium text-gray-700">
+						Id de Usuário
+					</label>
+					<input
+						value={id}
+						onChange={(e) => setId(e.target.value)}
+						type="text"
+						className="mt-1 block w-full px-4 py-2 border rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+						placeholder="Ex: 123"
+					/>
+				</div>
 
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Hash
-            </label>
-            <input
-              type="text"
-              className="mt-1 block w-full px-4 py-2 border rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Hash recebido por e-mail"
-            />
-          </div>
+				<div>
+					<label className="block text-sm font-medium text-gray-700">
+						Hash
+					</label>
+					<input
+						value={hash}
+						onChange={(e) => setHash(e.target.value)}
+						type="text"
+						className="mt-1 block w-full px-4 py-2 border rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+						placeholder="Hash recebido por e-mail"
+					/>
+				</div>
 
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Nova Senha
-            </label>
-            <input
-              type="password"
-              className="mt-1 block w-full px-4 py-2 border rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Digite a nova senha"
-            />
-          </div>
+				<div>
+					<label className="block text-sm font-medium text-gray-700">
+						Nova Senha
+					</label>
+					<input
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						type="password"
+						className="mt-1 block w-full px-4 py-2 border rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+						placeholder="Digite a nova senha"
+					/>
+				</div>
 
-          <div>
-            <label className="block text-sm font-medium text-gray-700">
-              Confirmação da Nova Senha
-            </label>
-            <input
-              type="password"
-              className="mt-1 block w-full px-4 py-2 border rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Confirme a nova senha"
-            />
-          </div>
+				<div>
+					<label className="block text-sm font-medium text-gray-700">
+						Confirmação da Nova Senha
+					</label>
+					<input
+						value={passwordConfirm}
+						onChange={(e) => setPasswordConfirm(e.target.value)}
+						type="password"
+						className="mt-1 block w-full px-4 py-2 border rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+						placeholder="Confirme a nova senha"
+					/>
+				</div>
 
-          <button
-            type="submit"
-            className="w-full bg-blue-600 text-white py-2 rounded-xl hover:bg-blue-700 transition-colors mt-2"
-          >
-            Alterar Senha
-          </button>
-      </div>
-    </div>
-  );
+				<button
+					onClick={handleSubmit}
+					type="submit"
+					className="w-full bg-blue-600 text-white py-2 rounded-xl hover:bg-blue-700 transition-colors mt-2"
+				>
+					Alterar Senha
+				</button>
+			</div>
+		</div>
+	);
+}
+
+async function handleSubmit() {
+	console.warn("função disparada com sucesso.")
 }
