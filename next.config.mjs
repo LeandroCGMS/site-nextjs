747bdfb39d7c6051e05110daa9badba5b68c3a38
LeCGMS <leandrocgms1986@outlook.com>
2025-04-25 21:03:57 -0400

    Mensagem automática do script "enviar" em 25/04/2025, às 21:03:57

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/nextjs', // Esta linha foi adicionada a essa configuração original
+};
 
 export default nextConfig;
